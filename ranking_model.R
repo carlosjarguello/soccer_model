3470da5341c2e40debe60e2bcbcbf74b94e4d280
Carlos J. Arguello <carlosjosear@gmail.com>
2018-06-11 09:19:46 -0400

    Fixed design matrix for no ties

--- ranking_model.R
+++ ranking_model.R
@@ -2,7 +2,7 @@
 
 library(tidyverse)
 
-soccer_data = read_csv("results.csv",
+soccer_data = read_csv("soccer_stats.csv",
                        cols(
                          date = col_date(format = ""),
                          home_team = col_character(),
@@ -20,51 +20,117 @@ soccer_data = read_csv("results.csv",
 
 soccer_data_no_ties = soccer_data %>%
   filter(home_score != away_score, date > "2015-01-01") %>%
-  mutate(w_loc = ifelse(neutral, 0, ifelse(home_score>away_score, 1, -1))) 
+  mutate(w_loc = ifelse(neutral, 0, ifelse(home_score>away_score, 1, -1)),
+         home_team = as.factor(home_team),
+         away_team = as.factor(away_team),
+         outcome = ifelse(home_score > away_score, 1, 0))
 
-soccer_data_no_ties = soccer_data_no_ties %>% 
-  group_by(home_team) %>%
-  filter(n() >= 5) %>%
-  ungroup() %>%
-  group_by(away_team) %>%
-  filter(n() >= 5) %>%
-  ungroup() %>%
-  filter(home_team %in% away_team) %>%
-  filter(away_team %in% home_team) %>% 
-  mutate(outcome = ifelse(home_score > away_score, 1, 0))
+all_teams = union(levels(soccer_data_no_ties$home_team), levels(soccer_data_no_ties$away_team))
 
 soccer_data_no_ties = soccer_data_no_ties %>%
-  mutate(outcome = ifelse(home_score > away_score, 1, 0),
-         home_team = as.factor(home_team),
-         away_team = as.factor(away_team)) %>%
+  mutate(home_team = factor(home_team, levels=all_teams),
+         away_team = factor(away_team, levels=all_teams)) %>%
   select(home_team, away_team, outcome)
 
-X_design = model.matrix(data = soccer_data_no_ties, ~ home_team) - 
-  model.matrix(data = soccer_data_no_ties, ~ away_team)
+X_design = model.matrix(data = soccer_data_no_ties, ~ home_team, 
+                        contrasts.arg=list(home_team=contrasts(soccer_data_no_ties$home_team, contrasts=F))) -
+  model.matrix(data = soccer_data_no_ties, ~ away_team, 
+               contrasts.arg=list(away_team=contrasts(soccer_data_no_ties$away_team, contrasts=F)))
 
+X_design = cbind(X_design[,-2], soccer_data_no_ties$outcome)
 X_design[,1] = 1
-colnames(X_design) <- c("home_Adv", as.character(sort(unique(soccer_data_no_ties$home_team))[-1]))
+colnames(X_design) <- c("home_adv", levels(soccer_data_no_ties$home_team)[-1], "game_outcome")
+design_data_no_ties = as.data.frame(X_design)
 
-
-model = glm.fit(X_design, soccer_data_no_ties$outcome, family = binomial())
-res = model$coefficients[order(model$coefficients, decreasing = TRUE)]
-
-res
+# model = glm.fit(X_design, soccer_data_no_ties$outcome, family = binomial())
+model = glm(data = design_data_no_ties, game_outcome ~ . -1, family = binomial())
+skill_coef = model$coefficients[order(model$coefficients, decreasing = TRUE)]
+summary(model)
+skill_coef
 
 # With ties:
 
-soccer_data_with_ties = soccer_data %>%
-  filter(date > "2016-01-01") %>%
-  group_by(home_team) %>%
-  filter(n() >= 3) %>%
-  ungroup() %>%
-  group_by(away_team) %>%
-  filter(n() >= 3) %>%
-  ungroup() %>%
-  filter(home_team %in% away_team) %>%
-  filter(away_team %in% home_team)
+# Miltinomial regression using Poisson trick:
 
-soccer_data_with_ties = soccer_data_with_ties  %>% 
+require(prefmod)
+
+teams_comb = expand.grid(all_teams, all_teams) %>% filter(Var1 != Var2)
+
+soccer_data_with_ties = soccer_data  %>% 
+  mutate(outcome = ifelse(home_score == away_score, "tie", 
+                          ifelse(home_score > away_score, "win", "loss")))
+
+teams_comb = teams_comb %>%
+  mutate()
+
+
+
+
+countries_SA = c("Argentina", 
+                 "Brasil", 
+                 "Colombia", 
+                 "Peru", 
+                 "Chile", 
+                 "Ecuador",
+                 "Bolivia",
+                 "Venezuela",
+                 "Uruguay",
+                 "Paraguay")
+
+soccer_south_america = soccer_data_with_ties %>% 
+  filter(home_team %in% countries_SA & away_team %in% countries_SA)
+
+soccer_south_america%>% View()
+
+des=llbt.design(soccer_south_america, nitems=10)
+
+
+
+
+# baseball example (Agresti, 2002, p. 437)
+# pseudodata for generating a design matrix
+# pseudo <- data.frame(rbind(c(rep(1, 21), cov = 1), 
+#                            c(rep(-1, 21), cov = 2)))
+# 
+# objnames <- c("MIL", "DET", "TOR", "NY", "BOS", "CLE", "BAL")
+# dsg <- llbt.design(pseudo, nitems = 7, objnames = objnames, cat.scovs = "cov")
+# dsg$mu <- gl(42, 2)
+# dsg$y <- baseball
+# dsg
+# 
+# mod.bb.0 <- gnm(y ~ MIL + DET + TOR + NY + BOS + CLE + BAL, 
+#                 eliminate = mu, family = poisson, data = dsg)
+# 
+# home <- c(rep(1:0, 21), rep(0:1, 21))
+# 
+# mod.bb.1 <- update(mod.bb.0, . ~ . + home)
+# 
+# anova(mod.bb.0, mod.bb.1)
+# mod.bb.1$coefficients
+# plogis(coef(mod.bb.1)["MIL"])
+# psych::logistic(coef(mod.bb.1)["MIL"])
+
+
+
+
+
+
+
+# With ties:
+
+
+# soccer_data_with_ties = soccer_data %>%
+#   filter(date > "2016-01-01") %>%
+#   group_by(home_team) %>%
+#   filter(n() >= 3) %>%
+#   ungroup() %>%
+#   group_by(away_team) %>%
+#   filter(n() >= 3) %>%
+#   ungroup() %>%
+#   filter(home_team %in% away_team) %>%
+#   filter(away_team %in% home_team)
+
+soccer_data_with_ties = soccer_data  %>% 
   mutate(outcome = ifelse(home_score == away_score, "tie", 
                           ifelse(home_score > away_score, "win", "loss")))
 
@@ -116,61 +182,4 @@ team_rankings = data.frame(teams, strength = result$par[-1]) %>%
   arrange(desc(strength))
 
 print(team_rankings)
-
-# Miltinomial regression using Poisson trick:
-
-soccer_data_with_ties = soccer_data %>%
-  filter(date > "2016-01-01") %>%
-  group_by(home_team) %>%
-  filter(n() >= 3) %>%
-  ungroup() %>%
-  group_by(away_team) %>%
-  filter(n() >= 3) %>%
-  ungroup() %>%
-  filter(home_team %in% away_team) %>%
-  filter(away_team %in% home_team)
-
-require(prefmod)
-
-countries_SA = c("Argentina", 
-                 "Brasil", 
-                 "Colombia", 
-                 "Peru", 
-                 "Chile", 
-                 "Ecuador",
-                 "Bolivia",
-                 "Venezuela",
-                 "Uruguay",
-                 "Paraguay")
-
-soccer_south_america = soccer_data_with_ties %>% 
-  filter(home_team %in% countries_SA & away_team %in% countries_SA)
-  
-soccer_south_america%>% View()
-
-des=llbt.design(soccer_south_america, nitems=10)
-
-
-# baseball example (Agresti, 2002, p. 437)
-
-# pseudodata for generating a design matrix
-d1 <- c(rep(0, 21), 1)
-d2 <- c(1, rep(0, 20), 2)
-d  <- data.frame(rbind(d1, d2))
-names(d) <- c(paste0("v", 1:21), "cov")
-
-# design matrix
-des5 <- llbt.design(d, nitems = 7,
-                    objnames = c("MIL", "DET", "TOR", "NY", "BOS", "CLE", "BAL"),
-                    cat.scov = "cov")
-des5$y  <- baseball
-des5$mu <- gl(42, 2)
-dpos     <- c(rep(1:0, 21), rep(0:1, 21))
-
-# fit model and display results
-res5 <- gnm(y ~ MIL+DET+TOR+NY+BOS+CLE+BAL + pos,
-            eliminate = mu, data = des5, family = poisson)
-w5   <- llbt.worth(res5)
-plot(w5)
-
 
